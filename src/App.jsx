70dead64d1dbacd7baf938730f6b4772a4c42f41
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-10-02 11:54:14 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
 
 const App = () => {
-  return (
-    <div>
-      app
-    </div>
-  )
-}
+  
+  return <main>
+    <Navbar />
+    <Hero />
+    <Sidebar/>
+    <Submenu />
+  </main>
+};
 
-export default App
+export default App;
